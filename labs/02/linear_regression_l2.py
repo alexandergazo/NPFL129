8da163096cb94f858e8aba66752a97381210d4f0
alexander <alexander@gazo.sk>
2020-10-15 16:55:13 +0200

    solved

--- labs/02/linear_regression_l2.py
+++ labs/02/linear_regression_l2.py
@@ -9,42 +9,41 @@ import sklearn.model_selection
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
+parser.add_argument("--plot", default=True, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.9, type=lambda x: int(x) if x.isdigit() else float(x),
+                    help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
-    # Load Boston housing dataset
     dataset = sklearn.datasets.load_boston()
-
-    # TODO: Split the dataset into a train set and a test set.
-    # Use `sklearn.model_selection.train_test_split` method call, passing
-    # arguments `test_size=args.test_size, random_state=args.seed`.
-
+    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(dataset.data,
+                                                                                                dataset.target,
+                                                                                                test_size=args.test_size,
+                                                                                                random_state=args.seed)
     lambdas = np.geomspace(0.01, 100, num=500)
-    # TODO: Using `sklearn.linear_model.Ridge`, fit the train set using
-    # L2 regularization, employing above defined lambdas.
-    # For every model, compute the root mean squared error
-    # (do not forget `sklearn.metrics.mean_squared_error`) and return the
-    # lambda producing lowest test error.
-    best_lambda = None
-    best_rmse = None
+
+    models = [sklearn.linear_model.Ridge(l) for l in lambdas]
+    predictions = map(lambda model: model.fit(train_data, train_target).predict(test_data), models)
+    rmses = map(lambda prediction: sklearn.metrics.mean_squared_error(test_target, prediction), predictions)
+    rmses = np.sqrt(np.asarray(list(rmses)))
+
+    best_lambda = lambdas[rmses.argmin()]
+    best_rmse = rmses.min()
 
     if args.plot:
-        # This block is not required to pass in ReCodEx, however, it is useful
-        # to learn to visualize the results.
-
-        # If you collect the respective results for `lambdas` to an array called `rmse`,
-        # the following lines will plot the result if you add `--plot` argument.
         import matplotlib.pyplot as plt
         plt.plot(lambdas, rmses)
         plt.xscale("log")
         plt.xlabel("L2 regularization strength")
         plt.ylabel("RMSE")
-        if args.plot is True: plt.show()
-        else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+        if args.plot is True:
+            plt.show()
+        else:
+            plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return best_lambda, best_rmse
 
--- labs/02/linear_regression_sgd.py
+++ labs/02/linear_regression_sgd.py
@@ -13,44 +13,42 @@ parser.add_argument("--batch_size", default=10, type=int, help="Batch size")
 parser.add_argument("--data_size", default=100, type=int, help="Data size")
 parser.add_argument("--epochs", default=50, type=int, help="Number of SGD iterations over the data")
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
-parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
+parser.add_argument("--plot", default=True, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.5, type=lambda x: int(x) if x.isdigit() else float(x),
+                    help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
-    # Create a random generator with a given seed
     generator = np.random.RandomState(args.seed)
-
-    # Generate an artifical regression dataset
     data, target = sklearn.datasets.make_regression(n_samples=args.data_size, random_state=args.seed)
-
-    # TODO: Append a constant feature with value 1 to the end of every input data
-
-    # TODO: Split the dataset into a train set and a test set.
-    # Use `sklearn.model_selection.train_test_split` method call, passing
-    # arguments `test_size=args.test_size, random_state=args.seed`.
-    train_data, test_data, train_target, test_target = None, None, None, None
-
-    # Generate initial linear regression weights
+    data = np.append(data, np.ones([data.shape[0], 1]), axis=1)
+    train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(data, target,
+                                                                                                test_size=args.test_size,
+                                                                                                random_state=args.seed)
     weights = generator.uniform(size=train_data.shape[1])
 
     train_rmses, test_rmses = [], []
     for epoch in range(args.epochs):
         permutation = generator.permutation(train_data.shape[0])
 
-        # TODO: Process the data in the order of `permutation`.
-        # For every `args.batch_size`, average their gradient, and update the weights.
-        # A gradient for example (x_i, t_i) is `(x_i^T weights - t_i) * x_i`,
-        # and the SGD update is `weights = weights - args.learning_rate * gradient`.
-        # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
+        for i in range(train_data.shape[0] // args.batch_size):
+            batch_data = train_data[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
+            batch_target = train_target[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
+            batch_prediction = np.matmul(batch_data, weights)
+            gradient = np.matmul(batch_data.T, (batch_prediction - batch_target)) / args.batch_size
+            weights = weights - args.learning_rate * gradient
 
-        # TODO: Append current RMSE on train/test to train_rmses/test_rmses.
+        train_prediction = np.matmul(train_data, weights)
+        test_prediction = np.matmul(test_data, weights)
+        train_rmses.append(sklearn.metrics.mean_squared_error(train_prediction, train_target, squared=False))
+        test_rmses.append(sklearn.metrics.mean_squared_error(test_prediction, test_target, squared=False))
 
-    # TODO: Compute into `explicit_rmse` test data RMSE when
-    # fitting `sklearn.linear_model.LinearRegression` on train_data.
-    explicit_rmse = None
+    explicit_prediction = sklearn.linear_model.LinearRegression().fit(train_data, train_target).predict(test_data)
+    explicit_rmse = sklearn.metrics.mean_squared_error(explicit_prediction, test_target, squared=False)
 
     if args.plot:
         import matplotlib.pyplot as plt
@@ -59,10 +57,13 @@ def main(args):
         plt.xlabel("Iterations")
         plt.ylabel("RMSE")
         plt.legend()
-        if args.plot is True: plt.show()
-        else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+        if args.plot is True:
+            plt.show()
+        else:
+            plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return test_rmses[-1], explicit_rmse
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
