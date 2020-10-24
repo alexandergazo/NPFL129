e2a661b8829940285542664dd2108ca127030ad5
alexander <alexander@gazo.sk>
2020-10-24 16:54:00 +0200

    solved

--- labs/03/logistic_regression_sgd.py
+++ labs/03/logistic_regression_sgd.py
@@ -4,8 +4,8 @@ import sys
 
 import numpy as np
 import sklearn.datasets
-import sklearn.metrics
-import sklearn.model_selection
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -16,8 +16,15 @@ parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning 
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+parser.add_argument("--test_size", default=0.5, type=lambda x: int(x) if x.isdigit() else float(x),
+                    help="Test set size")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 
 def main(args):
     # Create a random generator with a given seed
@@ -26,27 +33,24 @@ def main(args):
     # Generate an artifical regression dataset
     data, target = sklearn.datasets.make_classification(
         n_samples=args.data_size, n_features=2, n_informative=2, n_redundant=0, random_state=args.seed)
-
-    # TODO: Append a constant feature with value 1 to the end of every input data
-
-    # TODO: Split the dataset into a train set and a test set.
-    # Use `sklearn.model_selection.train_test_split` method call, passing
-    # arguments `test_size=args.test_size, random_state=args.seed`.
-
-    # Generate initial linear regression weights
+    data = np.append(data, np.ones((data.shape[0], 1)), axis=1)
+    train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=args.test_size,
+                                                                        random_state=args.seed)
     weights = generator.uniform(size=train_data.shape[1])
 
     for iteration in range(args.iterations):
         permutation = generator.permutation(train_data.shape[0])
 
-        # TODO: Process the data in the order of `permutation`.
-        # For every `args.batch_size`, average their gradient, and update the weights.
-        # You can assume that `args.batch_size` exactly divides `train_data.shape[0]`.
+        for i in range(train_data.shape[0] // args.batch_size):
+            batch_data = train_data[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
+            batch_target = train_target[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
+            gradients = - (batch_target - sigmoid(batch_data @ weights)).reshape(-1, 1) * batch_data
+            weights -= args.learning_rate * gradients.mean(axis=0)
 
-        # TODO: After the SGD iteration, measure the average loss and accuracy for both the
-        # train test and the test set. The loss is the average MLE loss (i.e., the
-        # negative log likelihood, or crossentropy loss, or KL loss) per example.
-        train_accuracy, train_loss, test_accuracy, test_loss = None, None, None, None
+        train_accuracy = accuracy_score(train_target, sigmoid(train_data @ weights) > 1 / 2)
+        train_loss = log_loss(train_target, sigmoid(train_data @ weights))
+        test_accuracy = accuracy_score(test_target, sigmoid(test_data @ weights) > 1 / 2)
+        test_loss = log_loss(test_target, sigmoid(test_data @ weights))
 
         print("After iteration {}: train loss {:.4f} acc {:.1f}%, test loss {:.4f} acc {:.1f}%".format(
             iteration + 1, train_loss, 100 * train_accuracy, test_loss, 100 * test_accuracy))
@@ -54,8 +58,8 @@ def main(args):
         if args.plot:
             import matplotlib.pyplot as plt
             if args.plot is not True:
-                if not iteration: plt.figure(figsize=(6.4*3, 4.8*(args.iterations+2)//3))
-                plt.subplot(3, (args.iterations+2)//3, 1 + iteration)
+                if not iteration: plt.figure(figsize=(6.4 * 3, 4.8 * (args.iterations + 2) // 3))
+                plt.subplot(3, (args.iterations + 2) // 3, 1 + iteration)
             xs = np.linspace(np.min(data[:, 0]), np.max(data[:, 0]), 50)
             ys = np.linspace(np.min(data[:, 1]), np.max(data[:, 1]), 50)
             predictions = [[1 / (1 + np.exp(-([x, y, 1] @ weights))) for x in xs] for y in ys]
@@ -64,10 +68,13 @@ def main(args):
             plt.scatter(train_data[:, 0], train_data[:, 1], c=train_target, marker="P", label="train", cmap=plt.cm.RdBu)
             plt.scatter(test_data[:, 0], test_data[:, 1], c=test_target, label="test", cmap=plt.cm.RdBu)
             plt.legend(loc="upper right")
-            if args.plot is True: plt.show()
-            else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+            if args.plot is True:
+                plt.show()
+            else:
+                plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return weights
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
--- labs/03/perceptron.py
+++ labs/03/perceptron.py
@@ -11,6 +11,8 @@ parser.add_argument("--data_size", default=100, type=int, help="Data size")
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
@@ -22,35 +24,36 @@ def main(args):
         n_samples=args.data_size, n_features=2, n_informative=2, n_redundant=0,
         n_clusters_per_class=1, flip_y=0, class_sep=2, random_state=args.seed)
     target = 2 * target - 1
+    data = np.append(data, np.ones((data.shape[0], 1)), axis=1)
 
-    # TODO: Append a constant feature with value 1 to the end of every input data
-
-    # Generate initial perceptron weights
-    weights = np.zeros(data.shape[1])
+    w = np.zeros(data.shape[1])
 
     done = False
     while not done:
         permutation = generator.permutation(data.shape[0])
 
-        # TODO: Implement the perceptron algorithm, notably one iteration
-        # over the training data in the order of `permutation`. During the
-        # training data iteration, perform the required update to the `weights`
-        # for incorrectly classified examples. If all training instances are
-        # correctly classified, set `done=True`, otherwise set `done=False`.
+        for x, t in zip(data[permutation], target[permutation]):
+            if w.T @ x * t <= 0:
+                w += t * x
+
+        done = np.all(data @ w * target > 0)
 
         if args.plot and not done:
             import matplotlib.pyplot as plt
             if args.plot is not True:
-                if not plt.gcf().get_axes(): plt.figure(figsize=(6.4*3, 4.8*3))
+                if not plt.gcf().get_axes(): plt.figure(figsize=(6.4 * 3, 4.8 * 3))
                 plt.subplot(3, 3, 1 + len(plt.gcf().get_axes()))
             plt.scatter(data[:, 0], data[:, 1], c=target)
             xs = np.linspace(*plt.gca().get_xbound() + (50,))
             ys = np.linspace(*plt.gca().get_ybound() + (50,))
-            plt.contour(xs, ys, [[[x, y, 1] @ weights for x in xs] for y in ys], levels=[0])
-            if args.plot is True: plt.show()
-            else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+            plt.contour(xs, ys, [[[x, y, 1] @ w for x in xs] for y in ys], levels=[0])
+            if args.plot is True:
+                plt.show()
+            else:
+                plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
-    return weights
+    return w
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
