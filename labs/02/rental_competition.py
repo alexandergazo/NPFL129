ff9d24051c4ce536bed7d1d6fc8f51e140abd2bd
alexander <alexander@gazo.sk>
2020-10-28 23:04:10 +0100

    tries

--- labs/02/rental_competition.py
+++ labs/02/rental_competition.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-from tqdm import tqdm
 import argparse
 import lzma
 import os
 import pickle
 import urllib.request
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, PolynomialFeatures
 from sklearn.compose import ColumnTransformer
@@ -14,6 +13,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, SGDRegressor
 import wandb
+from LinearRegressionSGD import LinearRegressionSGD
 
 
 class Dataset:
@@ -51,22 +51,57 @@ class Dataset:
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--predict", default="ridgeCV", type=str, help="Run prediction on given data")
+parser.add_argument("--predict", default=None, type=str, help="Run prediction on given data")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=32, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="rental_competition.model", type=str, help="Model path")
 parser.add_argument("--dev_size", default=0.10, type=float, help="Development set size")
-parser.add_argument("--plot", default=True, const=True, nargs="?", type=str, help="Plot the predictions")
-parser.add_argument("--batch_size", default=50, type=int, help="Batch size")
-parser.add_argument("--epochs", default=500, type=int, help="Number of SGD iterations over the data")
-parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
-parser.add_argument("--alpha", default=0, type=int, help="L2 rate")
+parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
+parser.add_argument("--batch_size", default=20, type=int, help="Batch size")
+parser.add_argument("--epochs", default=200, type=int, help="Number of SGD iterations over the data")
+parser.add_argument("--learning_rate", default=0.003, type=float, help="Learning rate")
+parser.add_argument("--alpha", default=0.01, type=int, help="L2 rate")
 
 
 def main(args):
     generator = np.random.RandomState(args.seed)
-    if args.predict == "ridgeCV":
+    if args.predict == "gridCV":
+        # wandb.init(project="npfl129", name="GridCV")
+        np.random.seed(args.seed)
+        dataset = Dataset()
+        dataset.data = np.append(dataset.data, np.ones([dataset.data.shape[0], 1]), axis=1)
+        train_data, dev_data, train_target, dev_target = train_test_split(dataset.data, dataset.target,
+                                                                          test_size=args.dev_size,
+                                                                          random_state=args.seed)
+        ints = np.all(train_data.astype(int) == train_data, axis=0)
+        ct = ColumnTransformer([("OneHot", OneHotEncoder(sparse=False, handle_unknown="ignore"), ints),
+                                ("Normalize", StandardScaler(), ~ints)])
+        feature_processing = Pipeline([
+            ("OH and Normalize", ct),
+            ("PF", PolynomialFeatures(3, include_bias=False))
+        ])
+        trainer = GridSearchCV(
+            LinearRegressionSGD(random_state=args.seed, epochs=60),
+            {'alpha': np.geomspace(1, 50, 30),
+             'learning_rate': np.geomspace(0.001, 0.1, 30),
+             'batch_size': [20, 50, 100, 200, 300]},
+            cv=KFold(5), verbose=3, n_jobs=2
+        )
+        trainer.fit(feature_processing.fit_transform(train_data), train_target)
+        model = Pipeline([("FP", feature_processing), ("LR", trainer)])
+        train_rmse = mean_squared_error(model.predict(train_data), train_target, squared=False)
+        print(train_rmse)
+        dev_rmse = mean_squared_error(model.predict(dev_data), dev_target, squared=False)
+        print(dev_rmse)
+        for epoch in range(args.epochs):
+            wandb.log({"train_rmse": train_rmse})
+            wandb.log({"dev_rmse": dev_rmse})
+        with lzma.open(args.model_path + "GR", "wb") as model_file:
+            pickle.dump(model, model_file)
+        with lzma.open("trainer", "wb") as trainer_file:
+            pickle.dump(trainer, trainer_file)
+    elif args.predict == "ridgeCV":
         wandb.init(project="npfl129", name="SGD_REGRESSOR")
         np.random.seed(args.seed)
         dataset = Dataset()
@@ -136,37 +171,31 @@ def main(args):
                                                                           random_state=args.seed)
         weights = generator.uniform(size=train_data.shape[1])
         train_rmses, dev_rmses = [], []
-        for _ in tqdm(range(args.epochs)):
+        for _ in range(args.epochs):
             permutation = generator.permutation(train_data.shape[0])
 
-            for i in range(train_data.shape[0] // args.batch_size):
+            for i in range(int(np.ceil(train_data.shape[0] / args.batch_size))):
                 batch_data = train_data[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
                 batch_target = train_target[permutation[i * args.batch_size:(i + 1) * args.batch_size]]
-                batch_prediction = np.matmul(batch_data, weights)
-                gradient = (np.matmul(batch_data.T, batch_prediction) + args.alpha * weights - np.matmul(batch_data.T,
-                                                                                                         batch_target)) / args.batch_size
+                batch_prediction = batch_data @ weights
+                gradient = (batch_data.T @ (batch_prediction - batch_target) + args.alpha * weights) \
+                           / batch_data.shape[0]
                 weights = weights - args.learning_rate * gradient
 
-            train_rmse = mean_squared_error(np.matmul(train_data, weights), train_target, squared=False)
-            dev_rmse = mean_squared_error(np.matmul(dev_data, weights), dev_target, squared=False)
+            train_rmse = mean_squared_error(train_data @ weights, train_target, squared=False)
+            dev_rmse = mean_squared_error(dev_data @ weights, dev_target, squared=False)
             wandb.log({"train_rmse": train_rmse, "dev_rmse": dev_rmse})
             train_rmses.append(train_rmse)
             dev_rmses.append(dev_rmse)
 
-        if args.plot:
-            import matplotlib.pyplot as plt
-            plt.plot(train_rmses, label="Train")
-            plt.plot(dev_rmses, label="Dev")
-            plt.xlabel("Epochs")
-            plt.ylabel("RMSE")
-            args.plot = "SGD"
-            if args.plot is True:
-                plt.show()
-            else:
-                plt.savefig(args.plot, transparent=True, bbox_inches="tight")
-                plt.savefig(args.plot + "2", transparent=False, bbox_inches="tight")
-                plt.show()
-
+        l = LinearRegression().fit(train_data, train_target)
+        l.coef_ = weights
+        model = Pipeline([
+            ("Feature Preprocessing", ct),
+            ("Polynomial Features 3rd degree", pf),
+            ("Lin", l)
+        ])
+        print(mean_squared_error(model.predict(dev_data), dev_target, squared=False))
         # Serialize the model.
         # with lzma.open(args.model_path, "wb") as model_file:
         #     pickle.dump(best_model, model_file + "ridge_alpha10")
