7286566620b2df166cec9af9d87d2c9ac489a55d
alexander <alexander@gazo.sk>
2020-11-04 17:12:45 +0100

    solved

--- labs/04/mlp_classification_sgd.py
+++ labs/04/mlp_classification_sgd.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import numpy as np
 import sklearn.datasets
 import sklearn.model_selection
+from mlp import *
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -15,204 +16,12 @@ parser.add_argument("--iterations", default=10, type=int, help="Number of iterat
 parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-parser.add_argument("--verbose", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--test_size", default=797, type=lambda x: int(x) if x.isdigit() else float(x),
                     help="Test set size")
 
 
 # If you add more arguments, ReCodEx will keep them with your default values.
-
-
-class LinearLayer:
-    def __init__(self, n_inputs, n_units, name, weights=None, biases=None):
-        self.n_inputs = n_inputs
-        self.n_units = n_units
-        self.name = name
-        self.W = weights
-        self.b = biases
-
-    def has_params(self):
-        return True
-
-    def forward(self, X):
-        assert X.shape[1] == self.n_inputs
-        return X @ self.W + self.b
-
-    def delta(self, Y, delta_next):
-        assert Y.shape == delta_next.shape
-        assert Y.shape[1] == self.n_units
-        return delta_next @ self.W.transpose()
-
-    def grad(self, X, delta_next):
-        assert X.shape[1] == self.n_inputs
-        assert delta_next.shape[1] == self.n_units
-        dW = X.transpose() @ delta_next
-        dW /= X.shape[0]
-        db = np.mean(delta_next, axis=0)
-        assert db.shape == self.b.shape, "{} {}".format(db.shape, self.b.shape)
-        assert dW.shape == self.W.shape, dW.shape + self.W.shape
-        return [dW, db]
-
-    def update_params(self, dtheta):
-        assert len(dtheta) == 2, len(dtheta)
-        dW, db = dtheta
-        assert dW.shape == self.W.shape, dW.shape
-        assert db.shape == self.b.shape, db.shape
-        self.W += dW
-        self.b += db
-
-    def initialize(self):
-        pass
-
-
-class ReLULayer:
-    def __init__(self, name):
-        self.name = name
-
-    def has_params(self):
-        return False
-
-    def forward(self, X):
-        # X[X < 0] = 0  # best performance but modifies X
-        XX = X * (X > 0)
-        return XX
-
-    def delta(self, Y, delta_next):
-        YS = (Y > 0)
-        return YS * delta_next
-
-
-def rowsoftmax(X):
-    max = np.max(X, axis=-1, keepdims=True)
-    numerator = np.exp(X - max)
-    denominator = np.sum(numerator, axis=-1, keepdims=True)
-    return numerator / denominator
-
-
-class SoftmaxLayer:
-    def __init__(self, name):
-        self.name = name
-
-    def has_params(self):
-        return False
-
-    def forward(self, X):
-        return rowsoftmax(X)
-
-    def delta(self, Y, delta_next):
-        assert Y.shape == delta_next.shape
-        return (-np.sum(delta_next * Y, axis=1, keepdims=True) + delta_next) * Y
-
-
-class MLP:
-    def __init__(self, n_inputs, layers, loss, output_layers=[]):
-        self.n_inputs = n_inputs
-        self.layers = layers
-        self.output_layers = output_layers
-        self.loss = loss
-        self.first_param_layer = layers[-1]
-        for l in layers:
-            if l.has_params():
-                self.first_param_layer = l
-                break
-
-    def propagate(self, X, output_layers=True, last_layer=None):
-        layers = self.layers + (self.output_layers if output_layers else [])
-        if last_layer is not None:
-            layer_names = [layer.name for layer in layers]
-            layers = layers[0: layer_names.index(last_layer) + 1]
-        for layer in layers:
-            X = layer.forward(X)
-        return X
-
-    def evaluate(self, X, T):
-        return self.loss.forward(self.propagate(X, output_layers=False), T)
-
-    def gradient(self, X, T):
-        Ys = []
-        message = X
-        for layer in self.layers:
-            message = layer.forward(message)
-            Ys.append(message)
-        g = {}
-        Y = Ys[-1]
-        d = self.loss.delta(Y, T)
-        idx = len(self.layers) - 1
-        for layer in reversed(self.layers):
-            Y = Ys[idx]
-            if layer.has_params():
-                if idx == 0:
-                    YY = X
-                else:
-                    YY = Ys[idx - 1]
-                g[layer.name] = layer.grad(YY, d)
-            d = layer.delta(Y, d)
-            idx -= 1
-        return g
-
-
-class LossCrossEntropy:
-    def __init__(self, name):
-        self.name = name
-
-    def forward(self, X, T):
-        assert X.shape == T.shape
-        return np.sum(-np.log(X) * T, axis=-1, keepdims=True)
-
-    def delta(self, X, T):
-        assert X.shape == T.shape
-        return - T / X
-
-
-def accuracy(Y, T):
-    p = np.argmax(Y, axis=1)
-    t = np.argmax(T, axis=1)
-    return np.mean(p == t)
-
-
-def train(net, train_data, train_target, batch_size=1, epochs=2, learning_rate=0.1, test_data=None, test_target=None,
-          verbose=False, generator=None):
-    """
-    Trains a network using vanilla gradient descent.
-    """
-    n_samples = train_data.shape[0]
-    assert train_target.shape[0] == n_samples
-    assert batch_size <= n_samples
-    run_info = defaultdict(list)
-
-    def process_info(epoch):
-        Y = net.propagate(train_data)
-        train_loss = net.loss.forward(Y, train_target)
-        train_accuracy = accuracy(Y, train_target)
-        run_info['train_loss'].append(train_loss)
-        run_info['train_accuracy'].append(train_accuracy)
-        if test_data is not None:
-            Y = net.propagate(test_data)
-            test_loss = net.loss.forward(Y, test_target)
-            test_accuracy = accuracy(Y, test_target)
-            run_info['test_loss'].append(test_loss)
-            run_info['test_accuracy'].append(test_accuracy)
-
-    process_info('initial')
-    for epoch in range(1, epochs + 1):
-        permutation = generator.permutation(train_data.shape[0]) if generator is not None else list(
-            range(train_data.shape[0]))
-        offset = 0
-        while offset < n_samples:
-            last = min(offset + batch_size, n_samples)
-            grads = net.gradient(np.asarray(train_data[permutation[offset:last]]),
-                                 np.asarray(train_target[permutation[offset:last]]))
-            for layer in net.layers:
-                if layer.has_params():
-                    gs = grads[layer.name]
-                    dtheta = [-learning_rate * g for g in gs]
-                    layer.update_params(dtheta)
-            offset += batch_size
-        process_info(epoch)
-        if verbose:
-            print("After iteration {}: train acc {:.1f}%, test acc {:.1f}%".format(
-                epoch, 100 * run_info['train_accuracy'][-1], 100 * run_info['test_accuracy'][-1]))
-    return run_info
+parser.add_argument("--verbose", default=False, action="store_true", help="Print training progress.")
 
 
 def one_hot(classes, x):
@@ -239,7 +48,7 @@ def main(args):
     biases = [np.zeros(args.hidden_layer), np.zeros(args.classes)]
 
     net = MLP(
-        n_inputs=train_data.shape[0],
+        n_inputs=train_data.shape[1],
         layers=[
             LinearLayer(n_inputs=train_data.shape[1], n_units=args.hidden_layer, name='Linear_1', weights=weights[0],
                         biases=biases[0]),
--- labs/04/mnist_competition.py
+++ labs/04/mnist_competition.py
@@ -7,6 +7,11 @@ import urllib.request
 import sys
 
 import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from scipy.ndimage import gaussian_filter, rotate, affine_transform, map_coordinates
+from sklearn.neural_network import MLPClassifier
+
 
 class Dataset:
     """MNIST Dataset.
@@ -14,6 +19,7 @@ class Dataset:
     The train set contains 60000 images of handwritten digits. The data
     contain 28*28=784 values in range 0-255, the targets are numbers 0-9.
     """
+
     def __init__(self,
                  name="mnist.train.npz",
                  url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
@@ -25,7 +31,7 @@ class Dataset:
         dataset = np.load(name)
         for key, value in dataset.items():
             setattr(self, key, value)
-        self.data = self.data.reshape([-1, 28*28]).astype(np.float)
+        self.data = self.data.reshape([-1, 28 * 28]).astype(np.float)
 
 
 parser = argparse.ArgumentParser()
@@ -36,25 +42,57 @@ parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="mnist_competition.model", type=str, help="Model path")
 
+
+def get_shift(size, sigma, alpha):
+    pos = np.random.uniform(size=size, low=-1, high=1)
+    pos = gaussian_filter(pos, sigma)
+    pos = pos / np.linalg.norm(pos) * alpha
+    return pos.round().astype('int')
+
+
+def transform_instance(img, predict, sigma=6, alpha=38, beta=15, gamma=20, cval=-1):
+    shift_x = get_shift((28, 28), sigma, alpha) + np.asarray([[i] * 28 for i in range(28)])
+    shift_y = get_shift((28, 28), sigma, alpha) + np.asarray([[i] * 28 for i in range(28)]).T
+    new_img = map_coordinates(img.reshape(28, 28), (shift_x, shift_y), order=2, cval=cval)
+
+    if predict == 1 or predict == 7:
+        beta /= 2
+    new_img = rotate(new_img, np.random.uniform(low=-beta, high=beta), cval=cval, output=np.array(img.reshape(28, 28)),
+                     order=1, reshape=False)
+
+    new_img = affine_transform(new_img, np.random.uniform(low=1 - gamma / 100, high=1 + gamma / 100) * np.eye(2),
+                               output_shape=(28, 28), cval=cval, order=1)
+    return new_img.flatten()
+
+
 def main(args):
     if args.predict is None:
-        # We are training a model.
+        from tqdm import tqdm
         np.random.seed(args.seed)
         train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        train.data = train.data / 255 * 2 - 1
+        train_data, test_data, train_target, test_target = train_test_split(
+            train.data, train.target, stratify=train.target, test_size=0.1, random_state=args.seed)
+        iters = 20
+        train_data_expanded = np.empty((train_data.shape[0] * iters, train_data.shape[1]))
+        train_target_expanded = np.empty((train_target.shape[0] * iters,))
+        for idx, instance in enumerate(tqdm(train_data)):
+            for i in range(iters):
+                train_data_expanded[idx * iters + i, :] = transform_instance(instance, train_target[idx])
+                train_target_expanded[idx * iters + i] = train_target[idx]
+        np.save("train_data_expanded", train_data_expanded)
+        np.save("train_target_expanded", train_target_expanded)
 
-        # If you trained one or more MLPs, you can use the following code
-        # to compress it significantly (approximately 12 times). The snippet
-        # assumes the trained MLPClassifier is in `mlp` variable.
-        # mlp._optimizer = None
-        # for i in range(len(mlp.coefs_)): mlp.coefs_[i] = mlp.coefs_[i].astype(np.float16)
-        # for i in range(len(mlp.intercepts_)): mlp.intercepts_[i] = mlp.intercepts_[i].astype(np.float16)
+        mlp = MLPClassifier((1000, 500), random_state=args.seed, verbose=10)
+        # mlp = MLPClassifier((1000, 500), random_state=args.seed, verbose=10, learning_rate_init=0.1, solver='sgd',
+        #                     activation='tanh', learning_rate='adaptive', momentum=0, max_iter=iters)
+        mlp.fit(train_data_expanded, train_target_expanded)
+        print(accuracy_score(test_target, mlp.predict(test_data)))
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
-            pickle.dump(model, model_file)
+            pickle.dump(mlp, model_file)
 
     else:
         # Use the model and return test set predictions, as either a Python list or a NumPy array.
@@ -63,9 +101,7 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = None
-
+        predictions = model.predict(test.data)
         return predictions
 
 
