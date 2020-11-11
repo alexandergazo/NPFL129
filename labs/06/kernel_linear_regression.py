0331b8cfb8c99c61d2fd223975ba72428fec5dda
alexander <alexander@gazo.sk>
2020-11-11 21:56:30 +0100

    solved poly kernel

--- labs/06/kernel_linear_regression.py
+++ labs/06/kernel_linear_regression.py
@@ -3,7 +3,7 @@ import argparse
 import sys
 
 import numpy as np
-import sklearn.metrics
+from sklearn.metrics import mean_squared_error
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -18,9 +18,26 @@ parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning 
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
 
+
 def main(args):
+    def kernel(x, y):
+        if args.kernel == 'rbf':
+            return np.exp(-args.kernel_gamma * np.linalg.norm(x - y) ** 2)
+        elif args.kernel == 'poly':
+            d, r = args.kernel_gamma * x * y + 1, 1
+            for _ in range(args.kernel_degree): r *= d
+            return r
+
+    def predict(X):
+        predictions = np.empty((X.shape[0], 1))
+        for i, row in enumerate(X):
+            predictions[i] = sum(beta * kernel(row, dictum) for beta, dictum in zip(betas, train_data))
+        return predictions
+
     # Create a random generator with a given seed
     generator = np.random.RandomState(args.seed)
 
@@ -33,35 +50,17 @@ def main(args):
 
     betas = np.zeros(args.data_size)
 
-    # TODO: Perform `args.iterations` of SGD-like updates, but in dual formulation
-    # using `betas` as weights of individual training examples.
-    #
-    # We assume the primary formulation of our model is
-    #   y = phi(x)^T w + bias
-    # and the loss in the primary problem is batched MSE with L2 regularization:
-    #   L = sum_{i \in B} 1/|B| * [1/2 * (phi(x_i)^T w + bias - target_i)^2] + 1/2 * args.l2 * w^2
-    #
-    # For `bias`, use explicitly the average of the training targets, and do
-    # not update it futher during training.
-    #
-    # Instead of using feature map `phi` directly, we use a given kernel computing
-    #   K(x, y) = phi(x)^T phi(y)
-    # We consider the following `args.kernel`s:
-    # - "poly": K(x, y; degree, gamma) = (gamma * x^T y + 1) ^ degree
-    # - "rbf": K(x, y; gamma) = exp^{- gamma * ||x - y||^2}
-    #
-    # After each iteration, compute RMSE both on training and testing data.
     train_rmses, test_rmses = [], []
-
     for iteration in range(args.iterations):
         permutation = generator.permutation(train_data.shape[0])
 
-        # TODO: Process the data in the order of `permutation`, performing
-        # batched updates to the `betas`. You can assume that `args.batch_size`
-        # exactly divides `train_data.shape[0]`.
+        for i in range(train_data.shape[0] // args.batch_size):
+            batch_indices = permutation[i * args.batch_size:(i + 1) * args.batch_size]
+            betas[batch_indices] = betas[batch_indices] + args.learning_rate / len(batch_indices) * (
+                        train_target[batch_indices] - predict(train_data[batch_indices]).flatten())
 
-        # TODO: Append RMSE on training and testing data to `train_rmses` and
-        # `test_rmses` after the iteration.
+        train_rmses.append(mean_squared_error(train_target, predict(train_data), squared=False))
+        test_rmses.append(mean_squared_error(test_target, predict(test_data), squared=False))
 
         if (iteration + 1) % 10 == 0:
             print("Iteration {}, train RMSE {:.2f}, test RMSE {:.2f}".format(
@@ -69,17 +68,20 @@ def main(args):
 
     if args.plot:
         import matplotlib.pyplot as plt
-        # If you want the plotting to work (not required for ReCodEx), compute the `test_predictions`.
-        test_predictions = None
+        test_predictions = predict(test_data)
 
         plt.plot(train_data, train_target, "bo", label="Train target")
         plt.plot(test_data, test_target, "ro", label="Test target")
+
         plt.plot(test_data, test_predictions, "g-", label="Predictions")
         plt.legend()
-        if args.plot is True: plt.show()
-        else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+        if args.plot is True:
+            plt.show()
+        else:
+            plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return train_rmses, test_rmses
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
